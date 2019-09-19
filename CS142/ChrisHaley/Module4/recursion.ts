0f26e3201ce5deccfa474152160c7f530cbc7b23
Chris Haley <haleychristopher@cityuniversity.edu>
2019-09-19 11:19:16 -0700

    Submission for Module 4

---
+++ CS142/ChrisHaley/Module4/recursion.ts
@@ -0,0 +1,14 @@
+function power(base: number, exponent: number): number {
+
+    if(exponent === 0) {
+        return 1; //base case
+    }
+    else if (exponent < 0) { //Error handling
+        throw new Error ('Exponent smaller than zero');
+    }
+    else{
+        return base * power(base, exponent - 1);
+    }
+}
+
+console.log(power(5, 3));
