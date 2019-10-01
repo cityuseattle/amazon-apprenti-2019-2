c35f389bb674957d8538d7ad6a208e39fcf618ee
Chris Haley <haleychristopher@cityuniversity.edu>
2019-10-01 12:05:07 -0700

    Submission for Module 2

--- CS242/ChrisHaley/Module1/Lab1/server.js
+++ CS242/ChrisHaley/Module1/Lab1/server.js
@@ -1,13 +1,14 @@
 'use strict'
 const http = require('http');
+
 const port = 4000;
 const hostname = 'localhost';
 
 const server = http.createServer((req, res) => {
     let content;
-    if(req.url === '/') content = 'This is my home page';
-    else if (req.url === '/address') content = '<h2>This is my address page</h2>';
-    else content = '404 Page not found.';
+    if (req.url === '/address') content = '<h2>This is my address page</h2>';
+    else if (req.url === '/') content = 'This is my home page';
+    else content = '404 Page not found.'
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html; charset = utf-8');
