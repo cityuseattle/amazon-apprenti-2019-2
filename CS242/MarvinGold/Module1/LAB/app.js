54a928d142964d93bd6266ade2a0c452d5588e34
Marvin G <goldmd@live.com>
2019-10-01 14:09:50 -0700

    Lab 1 edit

--- CS242/MarvinGold/Module1/LAB/app.js
+++ CS242/MarvinGold/Module1/LAB/app.js
@@ -6,12 +6,13 @@ const hostname = 'localhost';
 
 const server = http.createServer((req, res) => {
     let content;
-    if (req.url === '/address') content = '<h2> This is the ADDRESS page.</h2>';
-    else if (req.url === '/') content = '<h1> This is the HOME page. </h1>';
-    else content = '<h2>404 Page is not found.</h2>';
+    const ip = res.socket.remoteAddress;
+    if (req.url === '/address') content = '<h2>This is my address page.</h2>';
+    else if (req.url === '/') content = '<h1>This is the home page.</h1'
+    else content = '<h2>404 Page is not found.</h2>'
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.end('This is your first web server');
+    res.end(content);
     console.log(req.connection.remoteAddress);
 });
 
