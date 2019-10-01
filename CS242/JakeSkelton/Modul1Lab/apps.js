cf343fb4443933d882149d312f2075f57a4a4660
Skelton <skejakob@a483e70a6001.ant.amazon.com>
2019-10-01 09:04:51 -0700

    Lab Module 1

---
+++ CS242/JakeSkelton/Modul1Lab/apps.js
@@ -0,0 +1,17 @@
+'use strict'
+const http = require('http');
+
+const port = 4000;
+const hostname = 'localhost'
+
+const server = http.createServer((req, res) => {
+    let content;
+    if (req.url === '/address') content = '<h2>This is the address page.</h2>';
+    else if (req.url === '/') content = '<h1>This is the home page.</h1>'
+    req.connection.remoteAddress
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(content)
+});
+
+server.listen(port, () => console.log(`Server is running at http://${hostname}:${port}`));
