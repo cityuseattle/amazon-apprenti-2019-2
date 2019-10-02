45a31872900eb6d4081a4706724b164e6fdf4a82
Sabuna123 <sttimils@a483e702159c.ant.amazon.com>
2019-10-02 16:39:25 -0700

    Submission for Module 2

---
+++ CS242/Sabuna/Module2/src/middlewares/timestamp.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+const logging = (req: Request, res: Response, next: Function) => {
+    console.log(new Date());
+    next();
+
+};
+export default logging;
