bd13013e9c4b2b1cf042894b18450756afcd63e1
Christian <chris.gonz.fc@gmail.com>
2019-10-08 11:48:14 -0700

    Submission for Module 7

--- CS242/ChrisGonz/Module7/angular-app/src/app/app.module.ts
+++ CS242/ChrisGonz/Module7/angular-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomersclsComponent } from './customerscls/customerscls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CustomersComponent,
+    CustomersclsComponent
   ],
   imports: [
     BrowserModule,
