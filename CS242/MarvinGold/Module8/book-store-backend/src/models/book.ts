59d2afbc36fd8cf442f115e7d8ead763dff73cf9
goldmd <goldmarvin@cityuniversity.edu>
2019-10-09 11:59:05 -0700

    HOP 8 added

---
+++ CS242/MarvinGold/Module8/book-store-backend/src/models/book.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+type BookModel = mongoose.Document & {
+    title: string;
+    isbn: string;
+    author: string;
+    picture: string;
+    price: number;
+};
+
+//Schema time
+const schema = new mongoose.Schema({
+    title: { type: String, required: true },
+    isbn: { type: String, required: true },
+    author: { type: String, required: true },
+    //validation for picture URL
+    picture: { type: String, required: true },
+    price: { type: Number, required: true, min: 0 },
+});
+
+const Book = mongoose.model<BookModel>('Book', schema);
+
+//create book in DB
+export const addBook = (title: string, isbn: string, author: string, picture: string, price: number) => {
+    new Book({title, isbn, author, picture, price}).save();
+};
