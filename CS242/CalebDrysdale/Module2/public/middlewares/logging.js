36b272cbec4aa2078d37e3e9d63d293292cee280
CalebDrysdale <calebdrysdale@gmail.com>
2019-10-09 14:12:31 -0700

    submission for module 2

--- CS242/CalebDrysdale/Module2/public/middlewares/logging.js
+++ CS242/CalebDrysdale/Module2/public/middlewares/logging.js
@@ -4,6 +4,6 @@ var logging = function (req, res, next) {
     console.log('This will be logged before the controller running.');
     console.log(req.headers);
     next();
-    console.log('This will be logged after the controller running');
+    console.log('This will be logged after the controller running.');
 };
 exports.default = logging;
