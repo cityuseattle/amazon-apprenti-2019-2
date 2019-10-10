639d36b8d5899f0023e62a47ee868261614dbb71
Strait <mastrait@amazon.com>
2019-10-10 11:21:32 -0700

    LAB8 Submission

--- CS242/MarcStrait/Module8/book-store/src/app/app.module.ts
+++ CS242/MarcStrait/Module8/book-store/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { FormsModule } from "@angular/forms";
 import { MatSelectModule } from '@angular/material/select';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
@@ -16,11 +16,17 @@ import { MatDividerModule } from '@angular/material/divider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookFormComponent } from './book-form/book-form.component';
+import { BooksComponent } from './books/books.component';
+import { BookDetailDialogComponent } from './book-detail-dialog/book-detail-dialog.component';
+import { BirthdayCardFormComponent } from './birthday-card-form/birthday-card-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookFormComponent,
+    BooksComponent,
+    BookDetailDialogComponent,
+    BirthdayCardFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ CS242/MarcStrait/Module8/book-store/src/app/book-detail-dialog/book-detail-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-detail-dialog',
+  templateUrl: './book-detail-dialog.component.html',
+  styleUrls: ['./book-detail-dialog.component.css']
+})
+export class BookDetailDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
