d23c3bde49169b68c7daf6603367fca43aa4b42a
Jimenez <stevji@amazon.com>
2019-10-12 13:54:44 -0700

    Module9

--- CS242/StevenJimenez/Module8/book-store/src/app/app-routing.module.ts
+++ CS242/StevenJimenez/Module8/book-store/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BookFormComponent } from './book-form/book-form.component'; 
+import { BooksComponent } from './books/books.component';
+
 
 
 const routes: Routes = [
+{
+  path: '',
+  component: BooksComponent,
+},
   {
     path: 'admin/bookform',
     component: BookFormComponent,
