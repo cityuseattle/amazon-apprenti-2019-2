bcf7c0320518f14b9489a7c59f2dafbcab5f3ae3
Sabuna123 <sttimils@a483e702159c.ant.amazon.com>
2019-10-14 13:24:34 -0700

    Submission for moudle8

--- CS242/Sabuna/Module8/book-store-backend/src/app.ts
+++ CS242/Sabuna/Module8/book-store-backend/src/app.ts
@@ -1,21 +1,22 @@
 import express from 'express';
-import dotenv from 'dotenv'; 5.4K (gzipped: 2.3K)
+import dotenv from 'dotenv'; 
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors'; 5.2K (gzipped: 2.1K)
+import cors from 'cors'; 
 
 import addBookController from './controllers/add-book';
 dotenv.config();
 //Intialize MongoDB
 mongoose.connect (process.env.DB_URI as string, {useNewUrlParser: true});
 const db = mongoose.connection;
+
 const app = express();
 //
-app.use(cors({ origin: 'http://localhost:420'}));
+app.use(cors({ origin: 'http://localhost:4200'}));
 //
 app.use(bodyParser.json());
 //
 app.use(bodyParser.urlencoded({extended: true}));
-//
+//the router
 app.post('/book', addBookController)
-app.listen(process.env.port, () => console.log ('The server is running on http://localhost:${process.env.port}'));
+app.listen(process.env.port, () => console.log (`The server is running on http://localhost:${process.env.port}`));
