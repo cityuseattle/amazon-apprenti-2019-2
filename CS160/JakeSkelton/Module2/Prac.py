153c8148d98b7397cb94bee1169e25695aa87b71
Skelton <skejakob@a483e70a6001.ant.amazon.com>
2019-10-15 13:34:46 -0700

    Submission for Module 2

---
+++ CS160/JakeSkelton/Module2/Prac.py
@@ -0,0 +1,9 @@
+def greet(name):
+    print("Hello, " + name + "! Good Morning!")
+
+
+
+#pyhtonprint("what is your name?")
+name = input("what is ur name")
+
+greet(name)
---
+++ CS160/JakeSkelton/Module2/sort_tuples.py
@@ -0,0 +1,5 @@
+def first(n):
+    return n[0]
+
+def sort_list_first(tuple):
+    return sorted(tuples, key=first)
