9c3df8327786f5424f1397f198af7e24c97df502
Clark <mgclark@amazon.com>
2019-10-21 11:58:55 -0700

    Submission for Module 4

---
+++ CS160/miriam-clark/Module4/problem13.py
@@ -0,0 +1,14 @@
+#13. Given an array nums and a value val, remove all instances of that value and return the new length. 
+#Example:       Input: [0,1,2,2,3,0,4,2 ],   val=2 
+numList = [0,1,2,2,3,0,4,2]
+val = 2
+
+def removeIn(numList, val):
+    index = 0
+    count = numList.count(val)
+    while index < count:
+        numList.remove(val)
+        index += 1
+    print(len(numList))  
+        
+removeIn(numList, val)
---
+++ CS160/miriam-clark/Module4/problem2.py
@@ -0,0 +1,5 @@
+#2. Create the tuple ('k', 'b', 'c'), then create a list from that tuple. 
+#(Hint: the material needed to do this has been covered, but it's not entirely obvious)
+myTuple = ('k','b','c')
+print(myTuple)
+print(list(myTuple))
