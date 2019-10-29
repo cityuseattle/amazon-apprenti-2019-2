206d76f4b0396c10793169d5f4baba987e9fd252
Skelton <skejakob@a483e70a6001.ant.amazon.com>
2019-10-29 09:31:11 -0700

    Submission for Module 2

---
+++ CS260/JakeSkelton/Module2/cs260sandbox/hop/musicstore/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from django.contrib import admin
+
+from musicstore.payment import views as payment_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',TemplateView.as_view(template_name='homepage.html')),
+    url(r'^payment/$', include('musicstore.payment.urls',namespace="payment_ns")),
+    
+]
+
+
