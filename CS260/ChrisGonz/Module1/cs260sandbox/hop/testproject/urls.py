4de8b261682696d5935ed2b9844ec5bd3be86d0b
Christian <chris.gonz.fc@gmail.com>
2019-10-29 12:56:32 -0700

    Submission for Module 2

--- CS260/ChrisGonz/Module1/cs260sandbox/hop/testproject/urls.py
+++ CS260/ChrisGonz/Module1/cs260sandbox/hop/testproject/urls.py
@@ -24,6 +24,7 @@ from django.views.generic import TemplateView
 from testproject.payment import views as payment_views
 
 urlpatterns = [
+    #url(r'^cart/', )
     url(r'^admin/', admin.site.urls),
     url(r'^$',TemplateView.as_view(template_name='homepage.html')),
     url(r'^payment/', payment_views.pay)
