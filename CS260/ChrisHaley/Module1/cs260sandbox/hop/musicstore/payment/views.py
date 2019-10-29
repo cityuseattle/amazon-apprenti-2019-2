993426929398aab4d678a08db29d40fa44466db5
Chris Haley <haleychristopher@cityuniversity.edu>
2019-10-29 11:02:29 -0700

    Module 3 updates

--- CS260/ChrisHaley/Module1/cs260sandbox/hop/musicstore/payment/views.py
+++ CS260/ChrisHaley/Module1/cs260sandbox/hop/musicstore/payment/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 # Create your views here.
 
@@ -13,4 +14,19 @@ def pay(request, payment_type="Creditcard", discount=0):
     return render(request, 'payment/pay.html', {'address':address, 'payment_type':payment_type, 'discount':discount})
 
 def help(request):
-    return render(request, 'payment/help.html')
+    contact_name = "Chris"
+    contact_address = {'sector':'#385', 'city':'Sabody', 'planet':'Moon'}
+    contact_phone = ['123-456-7890', '987-654-3210']
+    contact_hours = ((0, 'Mon-Thur 7AM-12PM'), (1, 'Fri 1PM-5PM'), (2, 'Sat-Sun 7AM-10AM'))
+    # Package up all variables into a directory
+    contact_info = {'contact_name': contact_name, 'contact_address':contact_address, 'contact_phone':contact_phone, 'contact_hours':contact_hours}
+    return render(request, 'payment/help.html', contact_info)
+
+def refund(request):
+    contact_name = "Chris"
+    contact_address = {'sector':'#385', 'city':'Sabody', 'planet':'Moon'}
+    contact_phone = ['123-456-7890', '987-654-3210']
+    contact_hours = ((0, 'Mon-Thur 7AM-12PM'), (1, 'Fri 1PM-5PM'), (2, 'Sat-Sun 7AM-10AM'))
+    # Package up all variables into a directory
+    contact_info = {'contact_name': contact_name, 'contact_address':contact_address, 'contact_phone':contact_phone, 'contact_hours':contact_hours}
+    return TemplateResponse(request, 'payment/refund.html', contact_info)
