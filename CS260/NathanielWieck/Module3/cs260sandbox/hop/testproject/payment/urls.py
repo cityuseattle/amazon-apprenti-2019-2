ba65fb3fb64aa6e38ae2ebfc7f385953f57b3eac
Wieck <nwieck@f0189805e35c.ant.amazon.com>
2019-10-30 14:10:23 -0700

    Submission for Module3

---
+++ CS260/NathanielWieck/Module3/cs260sandbox/hop/testproject/payment/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+from testproject.payment import views as payment_views
+
+app_name = "payment_ns"
+
+urlpatterns = [
+    url(r'^$', payment_views.pay, name="pay"),
+    url(r'^help/$', payment_views.help, name="help"),
+    url(r'^refund/$', views.refund, name="refund"),
+    url(r'^(?P<payment_type>\D+)/',
+        views.pay, {'discount': '40'}, name="pay_details"),
+]
