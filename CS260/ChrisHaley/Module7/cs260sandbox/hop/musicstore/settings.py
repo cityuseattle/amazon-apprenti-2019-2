5a55193a11062cf0b489b762ea24b2d1e71d22f5
Chris Haley <haleychristopher@cityuniversity.edu>
2019-10-30 15:01:56 -0700

    updated for correct db engine

--- CS260/ChrisHaley/Module7/cs260sandbox/hop/musicstore/settings.py
+++ CS260/ChrisHaley/Module7/cs260sandbox/hop/musicstore/settings.py
@@ -104,8 +104,8 @@ WSGI_APPLICATION = 'musicstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'djangocs260_high',
         'USER': env('ORACLE_USER'),
         'PASSWORD': env('ORACLE_PWD'),
     }
