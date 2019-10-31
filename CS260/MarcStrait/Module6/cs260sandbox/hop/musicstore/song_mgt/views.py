817d527e95e0ebae3e4d0f1f548e04d9de8e1962
Strait <mastrait@amazon.com>
2019-10-31 14:23:19 -0700

    Submission for Module 6

---
+++ CS260/MarcStrait/Module6/cs260sandbox/hop/musicstore/song_mgt/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from . import forms
+# Create your views here.
+def add_song(request):
+    title = "Add a new song"
+    if request.method == 'POST':
+        # POST, generate form with data from request
+        form = forms.SongForm(request.POST)
+        if form.is_valid():
+            action='POST_SUCCESSFULL'
+        else:
+            action='POST_FAILED'
+    else:
+        action='GET'
+        form = forms.SongForm(auto_id=False,initial={'album_image':'http://'})
+    return render(request,'song/song_form.html',{'action':action,'form':form,'title':title})
