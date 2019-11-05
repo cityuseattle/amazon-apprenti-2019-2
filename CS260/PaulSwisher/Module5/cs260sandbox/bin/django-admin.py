e8544391519f8ef94e7e055de59870747d140b07
Swisher <pauswish@38f9d361a472.ant.amazon.com>
2019-11-05 13:54:29 -0800

    changes to the environ

--- CS260/PaulSwisher/Module5/cs260sandbox/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/pauswish/amazon-apprenti-2019-2/CS260/Module5/cs260sandbox/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
