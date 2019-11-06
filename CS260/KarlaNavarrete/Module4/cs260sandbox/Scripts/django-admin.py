0657d04226340d3f0eb35b29bb4750676a45178d
Navarrete <navkarl@amazon.com>
2019-11-06 14:44:42 -0800

    cleanup

--- CS260/KarlaNavarrete/Module4/cs260sandbox/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\navkarl\desktop\workingfolder\amazon-apprenti-2019\cs260\karlanavarrete\module4\cs260sandbox\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
