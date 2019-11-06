e8dd0474a2655757aa5120a328cbfac4c14b8759
Wieck <nwieck@f0189805e35c.ant.amazon.com>
2019-11-06 13:43:49 -0800

    Submission for Module 9

---
+++ CS260/NathanielWieck/Module9/hop/testproject/payment/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Payment(models.Model):
+    card_number = models.BigIntegerField(unique=True)
+    card_type = models.CharField(max_length=30)
+    billing_address = models.CharField(max_length=1000)
+
+    objects = models.Manager()
+
+    class Meta:
+        ordering = ['-card_number']
+
+def create_process(_card_number, _card_type, _billing_address):
+    payment = Payment(card_number=_card_number, card_type=_card_type, billing_address=_billing_address)
+
+    payment.full_clean()
+
+    payment.save()
+
+def fetch_all_pmt_methods_process():
+    return Payment.objects.in_bulk()
+
+def fetch_payment_method_process(_id):
+    return Payment.objects.get(id=_id)
+
+def edit_process(_id, _card_number, _card_type, _billing_address):
+    Payment.objects.filter(id=_id).update(card_number=_card_number, card_type=_card_type, billing_address=_billing_address)
+
+def delete_process(_id):
+    Payment.objects.filter(id=_id).delete()
