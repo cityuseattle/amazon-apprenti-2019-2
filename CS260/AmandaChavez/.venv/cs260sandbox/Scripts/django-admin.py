59ac160d6e3a287f5be9ed2f67660abac252d5d5
achavez-apprenti <amnchav@amazon.com>
2019-11-07 19:25:37 -0800

    Resubmitting.

--- CS260/AmandaChavez/.venv/cs260sandbox/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\amnchav\documents\city u\cs260\cs260\amandachavez\.venv\cs260sandbox\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
